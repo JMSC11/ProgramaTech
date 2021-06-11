ec527324668bff81f89409f6930dc50189942fc8
JMSC11 <misaelsc.ms@gmail.com>
2021-06-10 23:17:46 -0500

    Update cuenta.h

--- DMMD/GestionCuentas/cuenta.h
+++ DMMD/GestionCuentas/cuenta.h
@@ -13,6 +13,19 @@ class cuenta{
         string correo;
         int telefono;
     public:
-        cuenta();
+        cuenta(){
+
+        }
+        cuenta(int id, string usuario, string password, string nombre, string tipoCuenta, int edad, string correo, int telefono){
+            id = id;
+            usuario = usuario;
+            password = password;
+            nombre = nombre;
+            tipoCuenta = tipoCuenta;
+            edad = edad;
+            correo = correo;
+            telefono = telefono;
+        }
+
         ~cuenta();
 };
