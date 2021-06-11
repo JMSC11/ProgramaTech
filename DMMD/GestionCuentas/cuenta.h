948980dfd2faaf243e64d1ec8e53be189ac0a672
JMSC11 <misaelsc.ms@gmail.com>
2021-06-10 22:34:55 -0500

    Create cuenta.h
    clase cuenta

---
+++ DMMD/GestionCuentas/cuenta.h
@@ -0,0 +1,15 @@
+//Clase Cuenta
+
+class cuenta{
+    private:
+        int id;
+        string usuario;
+        string password;
+        string nombre;
+        string tipoCuenta;
+        int edad;
+        string correo;
+        int telefono;
+    public:
+        
+}
