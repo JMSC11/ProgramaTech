1e03aaa59ca19a46071e2cefdf95ee1709d8e9f9
Capa2124 <daniel.capa.21@outlook.com>
2021-06-14 21:39:24 -0500

    cambios general

--- DMMD/GestionCuentas/mainPruebas.cpp
+++ DMMD/GestionCuentas/mainPruebas.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 
 int main() {
-    cout << "Itto" << endl;
+    //cout << "Itto" << endl;
     ListaDeCuentas lCuentas;
     cuenta *c =  new cuenta(1, "diego", "123", "Diego Uriel", "Admin", 20, "ejemplo.com", 01234);
     lCuentas.registrar(c);
     lCuentas.mostrar("1");
-
     return 0;
 }
